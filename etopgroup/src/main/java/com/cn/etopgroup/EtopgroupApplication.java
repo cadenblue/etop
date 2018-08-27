332deaddcc4cb4087f7497ff70c223ea55c14ee9
何秋爽 <176806280@qq.com>
2018-08-27 10:55:47 +0800

    etop web初级架构 及集成了 assembly 打包，80端口

---
+++ etopgroup/src/main/java/com/cn/etopgroup/EtopgroupApplication.java
@@ -0,0 +1,12 @@
+package com.cn.etopgroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtopgroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtopgroupApplication.class, args);
+	}
+}
